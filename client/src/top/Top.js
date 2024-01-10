54664761882539cd48c3fe0780910f52581e7fdf
Juan Santelices <86135408+jdsantelicesl@users.noreply.github.com>
2024-01-09 18:53:26 -0800

    fixed first load bug

--- client/src/top/Top.js
+++ client/src/top/Top.js
@@ -1,110 +1,119 @@
-    import { useState, useEffect } from "react";
-    import fetchData from "../fetchData.js";
+import { useState, useEffect } from "react";
+import fetchData from "../fetchData.js";
 
-    import { logIn, getToken } from "../logIn.js";
+import { logIn, getToken } from "../logIn.js";
 
-    export default function Top() {
-        const [range, setRange] = useState("long_term");
-        const [type, setType] = useState("tracks");
+export default function Top() {
+    const [range, setRange] = useState("long_term");
+    const [type, setType] = useState("tracks");
 
-        const [content, setContent] = useState(null);
-        const [pending, setPending] = useState(true);
-        const [error, setError] = useState(null);
+    const [content, setContent] = useState(null);
+    const [pending, setPending] = useState(true);
+    const [error, setError] = useState(null);
 
-        const unpackToken = sessionStorage.getItem("token") === "null" ? null : sessionStorage.getItem("token");
-        const [token, setToken] = useState(unpackToken);
+    const unpackToken = sessionStorage.getItem("token") === "null" ? null : sessionStorage.getItem("token");
+    const [token, setToken] = useState(unpackToken);
 
-        let expiry = sessionStorage.getItem("expiry");
-        expiry = expiry === "null" ? null : expiry;
-        console.log("session token: ", token);
+    const unpackExpiry = sessionStorage.getItem("expiry");
+    const expiry = unpackExpiry === "null" ? null : parseFloat(unpackExpiry);
+    console.log("session token: ", token);
 
 
-        const unpackState = sessionStorage.getItem("state") === "null" ? null : sessionStorage.getItem("state");
-        const [state, setState] = useState(unpackState);
+    const unpackState = sessionStorage.getItem("state") === "null" ? null : sessionStorage.getItem("state");
+    const [state, setState] = useState(unpackState);
 
-        useEffect(() => {
-            console.log("log in update");
+    useEffect(() => {
+        console.log("log in update");
 
-            if (!state) {
-                logIn();
-                console.log("logging in");
-            }
-            
-            else if (state && !token) {
-                getToken();
-            }
-        }, [state, token]);
-
-        useEffect(() => {
-            console.log(expiry);
-            const current_time = new Date().getHours() + new Date().getMinutes()/60;
-            console.log(current_time);
-            if(expiry){
-                if(current_time - expiry >= 1 || current_time < expiry){
-                    console.log("reset fired");
-                    setState(null);
-                    setToken(null);
-                    sessionStorage.setItem("state", null);
-                    sessionStorage.setItem("token", null);
-                    sessionStorage.setItem("expiry", null);
-                    console.log("creds set to null");
-                }
-            }
-
-            console.log("token: ", token);
-            
-            if(token){
-                console.log("fetch fired");
-                fetchData(`https://api.spotify.com/v1/me/top/${type}?time_range=${range}&limit=50&offset=0`, updateData);
-                console.log("content: ", content);
-            }
-            
-        }, [range, type]);
-
-        const updateData = (newData, pend, err) => {
-            setContent(newData);
-            setPending(pend);
-            setError(err);
+        if (!state) {
+            logIn();
+            const newState = sessionStorage.getItem("state");
+            setToken(newState);
+            console.log("logging in");
         }
 
-        return (
-            <div className="Top">
-                <h1 className="headings">Search</h1>
-                <hr />
-                <p className="sHeading">Your Top Content:</p>
-                
-                <div style={{ textAlign: "center" }}>
-                    <span style={{ display: "inline-block", textAlign: "left" }}>Select Filters:</span>
-                    <label htmlFor="range"> Time Range: </label>
-                    <select id="range" value={range} onChange={(e) => setRange(e.target.value)}>
-                        <option value="short_term">Short Term</option>
-                        <option value="medium_term">Medium Term</option>
-                        <option value="long_term" selected>Long Term</option>
-                    </select>
+        else if (state && !token) {
+            getToken()
+                .then(() => {
+                const newToken = sessionStorage.getItem("token");
+                setToken(newToken);
+                console.log("newToken: ", newToken);
+            })
+            .catch(() =>{
+                console.log("Could not fetch token");
+            });
+        }
+    }, [state, token]);
 
-                    <label htmlFor="type"> Type: </label>
-                    <select id="type" value={type} onChange={(e) => setType(e.target.value)}>
-                        <option value="tracks" selected>Tracks</option>
-                        <option value="artists">Artists</option>
-                    </select>
-                    <button id="submit">Submit</button>
-                </div>
+    useEffect(() => {
+        console.log(expiry);
+        const current_time = new Date().getHours() + new Date().getMinutes() / 60;
+        console.log(current_time);
+        if (expiry) {
+            if (current_time - expiry >= 1 || current_time < expiry) {
+                console.log("reset fired");
+                setState(null);
+                setToken(null);
+                sessionStorage.setItem("state", null);
+                sessionStorage.setItem("token", null);
+                sessionStorage.setItem("expiry", null);
+                console.log("creds set to null");
+            }
+        }
 
-                <div style={{ paddingLeft: "5vw", paddingRight: "5vw" }}><hr /></div>
+        console.log("token: ", token);
 
-            { content && <div id="top">
-                    {content.items.map((song) => (
+        if (token) {
+            console.log("fetch fired");
+            fetchData(`https://api.spotify.com/v1/me/top/${type}?time_range=${range}&limit=50&offset=0`, updateData);
+        }
 
-                        <div className="list">
-                            {song.name}
-                        </div>
-                    
-                    ))}
-                    </div> }
-                
-            { pending && <p> Loading... </p>}
-            { error && <p> An error ocurred, please refresh. {error} </p>}
+    }, [range, type, token, state]);
 
-            </div>
-        );
+    const updateData = (newData, pend, err) => {
+        setContent(newData);
+        setPending(pend);
+        setError(err);
     }
+
+    return (
+        <div className="Top">
+            <h1 className="headings">Search</h1>
+            <hr />
+            <p className="sHeading">Your Top Content:</p>
+
+            <div style={{ textAlign: "center" }}>
+                <span style={{ display: "inline-block", textAlign: "left" }}>Select Filters:</span>
+                <label htmlFor="range"> Time Range: </label>
+                <select id="range" value={range} onChange={(e) => setRange(e.target.value)}>
+                    <option value="short_term">Short Term</option>
+                    <option value="medium_term">Medium Term</option>
+                    <option value="long_term" selected>Long Term</option>
+                </select>
+
+                <label htmlFor="type"> Type: </label>
+                <select id="type" value={type} onChange={(e) => setType(e.target.value)}>
+                    <option value="tracks" selected>Tracks</option>
+                    <option value="artists">Artists</option>
+                </select>
+                <button id="submit">Submit</button>
+            </div>
+
+            <div style={{ paddingLeft: "5vw", paddingRight: "5vw" }}><hr /></div>
+
+            {content && <div id="top">
+                {content.items.map((song) => (
+
+                    <div className="list">
+                        {song.name}
+                    </div>
+
+                ))}
+            </div>}
+
+            {pending && <p> Loading... </p>}
+            {error && <p> An error ocurred, please refresh. {error} </p>}
+
+        </div>
+    );
+}
